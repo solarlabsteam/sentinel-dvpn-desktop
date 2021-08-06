bf8fa877202b73be3ee8b110f7bf346fa3b3dd1d
IvanTurashov <ivan@solarlabs.ee>
2021-08-06 18:00:09 +0200

    added hot reloading to backend

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   lintOnSave: 'warning',
   pluginOptions: {
     electronBuilder: {
-      preload: 'src/main/preload.js'
+      preload: 'src/main/preload.js',
+      mainProcessWatch: ['src/main/**']
     }
   },
   devServer: {
