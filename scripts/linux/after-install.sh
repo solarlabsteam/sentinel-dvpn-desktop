19a1f1ee61aa1cd5f422decc2493366af08e049d
IvanTurashov <ivan@solarlabs.ee>
2021-10-12 17:12:44 +0300

    changed log text

--- scripts/linux/after-install.sh
+++ scripts/linux/after-install.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-echo 'Install wireguard-tools'
+echo 'Installing wireguard-tools'
 apt-get update && apt-get install openresolv wireguard-tools
 
-echo 'Copy sentinelcli'
+echo 'Copying sentinelcli'
 cp /opt/sentinel-dvpn-desktop/bin/linux/sentinelcli /usr/local/bin/sentinelcli
 
-echo 'Create the daemon'
+echo 'Creating the daemon'
 cp /opt/sentinel-dvpn-desktop/scripts/linux/sentinelcli-vpn.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable sentinelcli-vpn.service
--- scripts/linux/instal-dev-dependencies.sh
+++ scripts/linux/instal-dev-dependencies.sh
@@ -2,13 +2,13 @@
 
 DIRECTORY=`dirname $0`
 
-echo 'Install wireguard-tools'
+echo 'Installing wireguard-tools'
 apt-get update && apt-get install openresolv wireguard-tools
 
-echo 'Copy sentinelcli'
+echo 'Copying sentinelcli'
 cp $DIRECTORY/../../bin/linux/sentinelcli /usr/local/bin/sentinelcli
 
-echo 'Create the daemon'
+echo 'Creating the daemon'
 cp $DIRECTORY/../../scripts/linux/sentinelcli-vpn.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable sentinelcli-vpn.service
