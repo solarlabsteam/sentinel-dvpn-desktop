e9169ae40a81d09ec07ab355576e22c610eff6c9
IvanTurashov <ivan@solarlabs.ee>
2021-12-09 11:48:20 +0200

    fixed script-hooks

--- scripts/linux/after-install.sh
+++ scripts/linux/after-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'Copying sentinelcli'
-cp /opt/sentinel-dvpn-desktop/bin/linux/sentinelcli /usr/local/bin/sentinelcli
+cp /opt/Sentinel\ dVPN/bin/linux/sentinelcli /usr/local/bin/sentinelcli
 
 echo 'Creating the daemon'
-cp /opt/sentinel-dvpn-desktop/scripts/linux/sentinelcli-vpn.service /etc/systemd/system
+cp /opt/Sentinel\ dVPN/scripts/linux/sentinelcli-vpn.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable sentinelcli-vpn.service
 systemctl start sentinelcli-vpn.service
