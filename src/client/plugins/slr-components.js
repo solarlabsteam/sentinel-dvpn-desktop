93bebf2155a9de1364ad7a7a92f7a0a5245d47ea
IvanTurashov <ivan@solarlabs.ee>
2022-03-10 15:51:00 +0200

    unregistered unused slr components

--- src/client/plugins/slr-components.js
+++ src/client/plugins/slr-components.js
@@ -1,12 +1,9 @@
 import SlrButton from '@/client/components/ui/SlrButton'
 import SlrLoader from '@/client/components/ui/SlrLoader'
 import SlrLogo from '@/client/components/ui/SlrLogo'
-import SlrProgress from '@/client/components/ui/SlrProgress'
 import SlrIcon from '@/client/components/ui/SlrIcon'
-import SlrTimer from '@/client/components/ui/SlrTimer'
 import SlrTabs from '@/client/components/ui/SlrTabs'
 import SlrTab from '@/client/components/ui/SlrTabs/SlrTab'
-import SlrCheckbox from '@/client/components/ui/SlrCheckbox'
 import SlrPopper from '@/client/components/ui/SlrPopper'
 import SlrCopyButton from '@/client/components/ui/SlrCopyButton'
 import SlrTable from '@/client/components/ui/SlrTable'
@@ -22,12 +19,9 @@ export default function registerSlrComponents (app) {
   app.component('SlrButton', SlrButton)
   app.component('SlrLoader', SlrLoader)
   app.component('SlrLogo', SlrLogo)
-  app.component('SlrProgress', SlrProgress)
   app.component('SlrIcon', SlrIcon)
-  app.component('SlrTimer', SlrTimer)
   app.component('SlrTabs', SlrTabs)
   app.component('SlrTab', SlrTab)
-  app.component('SlrCheckbox', SlrCheckbox)
   app.component('SlrPopper', SlrPopper)
   app.component('SlrCopyButton', SlrCopyButton)
   app.component('SlrTable', SlrTable)
